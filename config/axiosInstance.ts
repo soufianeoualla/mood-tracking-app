dd34304b7e2a502d64da83df0dab8a9535d88efb
soufianeoualla <soufianeoualla0@gmail.com>
2025-06-25 21:56:54 +0100

    fix axios instance to use the base URL from the environment variable

--- config/axiosInstance.ts
+++ config/axiosInstance.ts
@@ -2,7 +2,7 @@ import useAuthStore from "@/store/useAuthStore";
 import axios, { AxiosError } from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL!,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
